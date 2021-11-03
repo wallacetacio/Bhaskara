83949b41689067220200fabfbaf0abe6c41fa9d5
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-02 22:39:30 -0300

    Adiciona resolução ao exercício Bhaskara ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Bhaskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c, delta, x1, x2;
+            
+
+            Console.WriteLine("-- Equação do Segundo Grau --");
+            
+            Console.Write("(a): ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("(b): ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("(c): ");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+
+            if(a == 0){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Não é uma equação de segundo grau!");
+                Console.ResetColor();
+            }
+            else
+            {
+                delta = (b * b) - 4 * a * c;
+            
+
+            if(delta < 0){
+                Console.WriteLine($"Como delta = {delta:N2}, a equação não possui raízes reais!");
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+                x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"X1 = {x1:N2} e X2 = {x2:N2}");
+                Console.ResetColor();
+            }
+            }
+            }
+    }
+}
